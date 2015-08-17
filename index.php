d8bf78842c40179c88dd8736c67baf2beaa5c946
MonteGramo <montegramo13@gmail.com>
2015-08-17 23:00:57 +0200

    Design of page
    Changed design to be similar to main site

--- index.php
+++ index.php
@@ -2,29 +2,91 @@
 	<head>
 		<title>4CDM</title>
 		<style>
-			table, td ,tr {
-				border: solid black 1px;
-				padding: 4px;
-				margin: 2px;
+			body {
+				color: #003366;
+				background-color: #000000;
+				font-weight: bold;
+				font: 16px verdana, geneva, lucida, 'lucida grande', arial, helvetica, sans-serif;
+			}
+			
+			div {
+				display: inline-block;
+				background-color: white;
+				border-top-left-radius: 7px;
+				border-top-right-radius: 7px;
+				padding-left: 6px;
+				padding-right: 6px;
+				padding-bottom: 16px;
+			}
+			
+			table {
+				padding: 2px;
+			}
+		
+			table, td {
+								
+				
 				border-spacing: 0px;
 				border-collapse: collapse;
 			}
 		
+			th {
+				font-weight: bold;	
+				padding: 5px;
+				color: white;
+				
+			}
+			
+			tr {
+				background-color: #666666;
+			}
+			
 			td {
+				background-color: #F4F4F4;
+				font-weight: normal;
+				padding: 18px;
 				text-align: center;
-		
 			}
 		
+			a {
+				font-weight: bold;
+				text-decoration: none;
+				color: #003366;
+			}
+			
+			a:hover {
+				text-decoration: underline;
+				color: #F76300;
+			}
 		
-		
+			#line1 th {
+				color: white;
+				padding: 18px;
+				background-color: #FD8C40;
+				border-top-left-radius: 7px;
+				border-top-right-radius: 7px;
+				border-top: solid white 4px;
+				border-bottom: solid #FFD10C 4px;
+			}
+			
+			#tr_line2 {
+				background-color: #666666; 
+				color: white;
+			}
 		</style>
 	<head>
 	<body>
 		<center>
+		<div>
 		<table>
-			<th><b>ODTWARZACZE</b></th>
-			<th><b>POBRANIA</b></th>
-			<th><b>USUN</b></th>
+		<tr id="line1">
+			<th  colspan="3">4Clubbers Download Manager</th>
+		</tr>
+		<tr id="line2">
+			<th>ODTWARZACZE</th>
+			<th>POBRANIA</th>
+			<th>USUN</th>
+		<tr>
 		<?php
 			require_once ('mysql_connect.php');
 			
@@ -56,6 +118,7 @@
 			}
 		?>	
 		</table>
+		</div>
 		</center>
 	</body>
 </html>
